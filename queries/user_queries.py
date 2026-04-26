492fb15903db66d1d6d03319216152030bf414e1
Nitesh Raj <rajkalamas@gmail.com>
2026-04-26 22:41:53 +0800

    Add new files

--- queries/user_queries.py
+++
@@ -1,8 +0,0 @@
-from db_connection import get_connection
-
-def create_user(username, password):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-    conn.commit()
-    conn.close()
